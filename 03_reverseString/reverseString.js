b28b271c7916fa69f0f0b9831f1d62d72676fa9b
Underdog Developer <underdogdeveloper@underdogdeveloperresouce.com>
2023-11-02 09:20:36 -0400

    completed reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = [];    
+    for (let i = input.length; i >= 0; --i) {
+        output.push(input[i]);
+    }
+    return output.join('');
 };
 
 // Do not edit below this line
