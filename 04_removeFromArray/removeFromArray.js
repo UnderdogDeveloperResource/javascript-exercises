70f3e87b28bf89cd8c0c3a78e031c319a4e5d9f4
Underdog Developer <underdogdeveloper@underdogdeveloperresouce.com>
2023-11-02 12:39:19 -0400

    completed remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; ++i) {
+        let index = arr.indexOf(arguments[i]);
+        if(index < 0) {
+            continue;
+        }
 
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
