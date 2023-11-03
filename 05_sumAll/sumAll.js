bab5f9289290d9b421a8da40634e07d58c6065be
Underdog Developer <underdogdeveloper@underdogdeveloperresouce.com>
2023-11-03 07:51:15 -0400

    completed sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Negative or non-numeric numbers are not allowed
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    if(typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
 
+    // Swap numbers around if the first is greater
+    if (num1 > num2) {
+        var temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
+    // sum all numbers between num1 and num2
+    let sum = num1;
+    while (num1 < num2) {
+        ++num1;
+        sum += num1;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
