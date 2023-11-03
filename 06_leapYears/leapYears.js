891705d6fa65f01511b457e911c08d5b7d03b095
Underdog Developer <underdogdeveloper@underdogdeveloperresouce.com>
2023-11-03 08:03:50 -0400

    completed leap years exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {        
+    return isDivisibleBy(year, 4) && (!isDivisibleBy(year, 100) || isDivisibleBy(year, 400));
 };
+
+function isDivisibleBy (num, divisor) {
+    return num % divisor === 0;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
