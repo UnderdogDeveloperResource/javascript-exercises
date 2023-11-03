17c7497e7b5c11645717398334c747084c575898
Underdog Developer <underdogdeveloper@underdogdeveloperresouce.com>
2023-11-03 08:21:53 -0400

    completed temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // F = (x - 32) * 5/9
+  let converted = (temp - 32) * 5/9;
+  return round(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // C = (x * 9/5 + 32)
+  let converted = temp * 9/5 + 32;
+  return round(converted);
 };
+
+function round(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
